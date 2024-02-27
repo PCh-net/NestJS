3914e6a0a6f98d1b0c970a510319787a85326b5f
Piotr <khazal@wp.pl>
2024-02-27 15:05:16 +0100

    Add endpoint all and id authors

--- src/authors/authors.controller.ts
+++ src/authors/authors.controller.ts
@@ -1,7 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, NotFoundException, Param } from '@nestjs/common';
 import { PrismaService } from 'src/shared/services/prisma.service';
+import { AuthorsService } from './authors.service';
+import { ParseUUIDPipe } from '@nestjs/common';
 
 @Controller('authors')
 export class AuthorsController {
-  constructor(private prismaService: PrismaService) {}
+  constructor(private authorsService: AuthorsService) {}
+  @Get('/')
+  getAll(): any {
+    return this.authorsService.getAll();
+  }
+
+  @Get('/:id')
+  async getById(@Param('id', new ParseUUIDPipe()) id: string) {
+    const product = await this.authorsService.getById(id);
+    if (!product) throw new NotFoundException('Author not found');
+    return product;
+  }
 }
