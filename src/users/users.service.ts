70eb97b7a71b4efe42f4c92bcc2c9c7679c0c6ff
Piotr <khazal@wp.pl>
2024-03-02 09:56:39 +0100

    users-prisma

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, ConflictException } from '@nestjs/common';
-import { PrismaService } from 'src/shared/services/prisma.service';
+import { PrismaService } from 'src/prisma/prisma.service';
 import { Role, User } from '@prisma/client';
 
 @Injectable()
